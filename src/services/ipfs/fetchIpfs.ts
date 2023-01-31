02a2a25a17d3aeb08b53ded9fd7131b25209aca4
thisisommore <ommore501@gmail.com>
2023-01-31 13:33:26 +0000

    fetchIpfs: check for prefix

--- src/services/ipfs/fetchIpfs.ts
+++ src/services/ipfs/fetchIpfs.ts
@@ -2,17 +2,18 @@
 export async function fetchIpfs<T>(ipfsUrl: string, isString?: Boolean): Promise<T | undefined> {
     const url = ipfsToHttps(ipfsUrl)
     const fetchRes = await fetch(url);
+    if (!ipfsUrl.startsWith("ipfs://")) return undefined
     try {
         if (isString) {
             const fetchText = await fetchRes.text();
             return fetchText as T
-
         }
         const fetchJson = await fetchRes.json();
         return fetchJson as T
-
     } catch (error) {
-        return fetchRes.text() as T
+        console.log("error max");
+
+        return undefined
     }
 }
 export function ipfsToHttps(ipfsUrl: string) {
