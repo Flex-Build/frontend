f23ace91b5b0eb43d58f23304248bf07d21ba453
thisisommore <ommore501@gmail.com>
2023-02-02 07:01:21 +0000

    Integrate properties in Exp

--- pages/DesignYourSite/TestComponents/Exp.tsx
+++ pages/DesignYourSite/TestComponents/Exp.tsx
@@ -1,12 +1,20 @@
 import { fetchIpfs } from "@/src/services/ipfs/fetchIpfs";
 import React, { useEffect, useState } from "react";
+import { createNodeArray } from "typescript";
 import Properties from "../properties/Properties";
 
 type Props = {
   ipfsHash: string;
 };
+
 const Exp = (p: Props) => {
   const [htmlString, setHtmlString] = useState("");
+  const [display, setDisplay] = useState(false);
+  const [cssProps, setCssProps] = useState<React.CSSProperties>({
+    width: "80%",
+    padding: "10px",
+    borderRadius: "10px",
+  });
 
   useEffect(() => {
     fetchIpfs<string>(p.ipfsHash, true).then((e) => {
@@ -21,8 +29,15 @@ const Exp = (p: Props) => {
         dangerouslySetInnerHTML={{
           __html: htmlString,
         }}
+        style={cssProps}
+        onClick={() => setDisplay(true)}
       ></div>
-      <Properties />
+      <Properties
+        visible={display}
+        onCancel={() => setDisplay(false)}
+        onStyleChange={setCssProps}
+        cssProps={cssProps}
+      />
     </>
   );
 };
--- pages/DesignYourSite/properties/Properties.tsx
+++ pages/DesignYourSite/properties/Properties.tsx
@@ -1,41 +1,79 @@
 import React, { useState } from "react";
 import styles from "./Properties.module.scss";
 
-function Properties() {
-  const [display, setDisplay] = useState(false);
-
+type Props = {
+  onCancel: () => void;
+  cssProps: React.CSSProperties;
+  onStyleChange: (cssProps: React.CSSProperties) => void;
+  visible: boolean;
+};
+const Properties: React.FC<Props> = (p) => {
   return (
-    <>    {display ?
-      <div className={styles.propertiesSection}>
-        <div className={styles.propAndBtn}>
-        <h6 className={styles.propertyname}>Properties</h6>
-        <button onClick={() => (display ? setDisplay(false) : setDisplay(true))} className={styles.cross}>X</button>
+    <>
+      {p.visible && (
+        <div className={styles.propertiesSection}>
+          <div className={styles.propAndBtn}>
+            <h6 className={styles.propertyname}>Properties</h6>
+            <button onClick={p.onCancel} className={styles.cross}>
+              X
+            </button>
+          </div>
+          <hr className={styles.hl} />
+          <div className={styles.propertiesContainer}>
+            <div className="flex m-2">
+              <p className={styles.PropertyLable}>Width</p>
+              <input
+                className={styles.inputParemeter}
+                type="string"
+                value={p.cssProps.width}
+                onChange={(e) => {
+                  const new_css: React.CSSProperties = {
+                    ...p.cssProps,
+                    width: e.target.value,
+                  };
+                  p.onStyleChange(new_css);
+                }}
+              ></input>
+            </div>
+
+            <div className="flex m-2">
+              <p className={styles.PropertyLable}>Padding</p>
+              <input
+                className={styles.inputParemeter}
+                type="string"
+                value={p.cssProps.padding}
+                onChange={(e) => {
+                  const new_css: React.CSSProperties = {
+                    ...p.cssProps,
+                    padding: e.target.value,
+                  };
+                  p.onStyleChange(new_css);
+                }}
+              ></input>
+            </div>
+
+            <div className="flex m-2">
+              <p className={styles.PropertyLable}>Rounded</p>
+              <input
+                className={styles.inputParemeter}
+                value={p.cssProps.padding}
+                onChange={(e) => {
+                  const new_css: React.CSSProperties = {
+                    ...p.cssProps,
+                    borderRadius: e.target.value,
+                  };
+                  p.onStyleChange(new_css);
+                }}
+                type="string"
+              ></input>
+            </div>
+          </div>
+
+          <button className={styles.publishBtn}>publish</button>
         </div>
-        <hr className={styles.hl}/>
-        <div className={styles.propertiesContainer}>
-          <div className="flex m-2">
-            <p className={styles.PropertyLable}>width</p>
-            <input className={styles.inputParemeter} type="number"></input>
-          </div>
-
-          <div className="flex m-2">
-            <p className={styles.PropertyLable}>Padding</p>
-            <input className={styles.inputParemeter} type="number"></input>
-          </div>
-
-          <div className="flex m-2">
-            <p className={styles.PropertyLable}>Rounded</p>
-            <input className={styles.inputParemeter} type="number"></input>
-          </div>
-        </div>
-
-        <button className={styles.publishBtn} >publish</button>
-      </div>
-      :
-      <button className={styles.displayBtn} onClick={() => (display ? setDisplay(false) : setDisplay(true))}>properties</button>}
-      </>
-
+      )}
+    </>
   );
-}
+};
 
 export default Properties;
