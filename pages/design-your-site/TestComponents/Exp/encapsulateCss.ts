eecfc8ab1e491db1ff17552da31c54d097df84f0
thisisommore <ommore501@gmail.com>
2023-02-03 08:05:59 +0000

    encapsulate css: fix incorrect syntax after processing

--- pages/design-your-site/TestComponents/Exp/encapsulateCss.ts
+++ pages/design-your-site/TestComponents/Exp/encapsulateCss.ts
@@ -1,40 +1,46 @@
 export const encapsulateCss = (htmlString: string, id: string) => {
+
     const lines = htmlString.split("\n");
     const styleTag = lines.findIndex((e) => e.includes("<style>"));
     if (styleTag == -1) {
         return htmlString;
     }
 
-    let classNames: string[] = [];
+    let classNames = new Set<string>()
     for (let index = styleTag; index < lines.length; index++) {
         const line = lines[index];
         if (line.includes("{")) {
             if (line.includes(".")) {
                 const _classNames = line
-                    .slice(1, -1)
                     .split(".")
-                    .map((e) => e.trim());
-                classNames = classNames.concat(_classNames.filter((e) => e != " "));
+                    .map(e => e.replace("{", ""))
+                    .map((e) => e.trim())
+                    .map(e => {
+                        return e.split(":")[0]
+                    });
+                _classNames.forEach((e) => { if (e != " " && e != "") classNames.add(e) })
             }
         }
     }
-
-    for (let cN of classNames) {
+    console.log(classNames);
+    classNames.forEach(cN => {
         // const regexExp = `/\\.${cN}|"${cN}| ${cN} |${cN}"/i`;
         const regexExpStr = `\\.${cN}`;
         const regexExp = new RegExp(regexExpStr, "g");
         htmlString = htmlString.replaceAll(regexExp, "." + cN + id);
-    }
-    for (let cN of classNames) {
-        const regexExpStr = `"${cN}`;
+
+    })
+    classNames.forEach(cN => {
+        const regexExpStr = `class="${cN}`;
         const regexExp = new RegExp(regexExpStr, "g");
-        htmlString = htmlString.replaceAll(regexExp, `"` + cN + id);
-    }
-    for (let cN of classNames) {
-        const regexExpStr = ` ${cN}`;
+        htmlString = htmlString.replaceAll(regexExp, `class="` + cN + id);
+    })
+    classNames.forEach(cN => {
+        const regexExpStr = ` ${cN} `;
         const regexExp = new RegExp(regexExpStr, "g");
         htmlString = htmlString.replaceAll(regexExp, " " + cN + id + " ");
-    }
+        console.log(htmlString);
+    })
 
     return htmlString;
 };
