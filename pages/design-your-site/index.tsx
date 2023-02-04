ee1fc05bf6be3fb5dda3e177a912ee8e56fd8157
shashankpatil125 <shashankpatil125@gmail.com>
2023-02-04 13:32:08 +0530

    popup

--- pages/design-your-site/index.tsx
+++ pages/design-your-site/index.tsx
@@ -1,5 +1,4 @@
 import Navbar from "@/components/Navbar";
-import { publish_site } from "@/src/apis/axios";
 import React, { useContext, useState } from "react";
 import { DragPreviewImage } from "react-dnd";
 import styles from "./DesignYourSite.module.scss";
@@ -7,51 +6,43 @@ import LeftContainer from "./leftContainer/LeftContainer";
 import Properties from "./properties/Properties";
 import Rightcontainer from "./rightContainer/Rightcontainer";
 
-function DesignYourSite() {
-  const [siteName, setSiteName] = useState("");
+function index() {
+  const [siteName, setSiteName] = useState('');
   const [vis, setvis] = useState(false);
-  const [htmlString, setHtmlString] = useState("");
   return (
     <div className={styles.designpage}>
       <div className="h-full w-full">
         <Navbar />
         <div className={styles.maincontainer}>
           <LeftContainer />
-          <Rightcontainer htmlgen={setHtmlString} />
+          <Rightcontainer />
         </div>
-        <button className={styles.publishBtn} onClick={() => setvis(true)}>
-          Publish
-        </button>
+        <button className={styles.publishBtn} onClick={() => (setvis(true))} >publish</button>
 
-        {vis ? (
-          <div className="h-full w-full backdrop-blur-sm absolute top-0">
-            <div className="h-1/6 mx-auto w-2/6 bg-black flex">
-              <input
-                placeholder="Site Name"
-                type="text"
-                className="h-10"
-                onChange={(e) => setSiteName(e.target.value)}
-              ></input>
-              <button
-                className="bg-yellow-500 h-10 px-10"
-                onClick={() => {
-                  publish_site(siteName, [], htmlString);
-                }}
-              >
-                Ok
-              </button>
-              <button
-                className="bg-lime-500 h-10 px-10"
-                onClick={() => setvis(false)}
-              >
-                Cancel
-              </button>
+        {vis ?
+          <div className={styles.popup1}>
+            <div className={styles.popup2}>
+              <input placeholder="Site Name" type="text" className={styles.popup3} onChange={e => setSiteName(e.target.value)}></input>
+              <div className={styles.popcenter}>
+                <button className={styles.popup4}>Ok</button>
+                <button className={styles.popup5} onClick={() => setvis(false)}>Cancel</button>
+              </div>
             </div>
-          </div>
-        ) : null}
+          </div> : null}
+
+
+        {/* {vis?
+        <div className="h-full w-full backdrop-blur-sm absolute top-0">
+        <div className="h-1/6 mx-auto w-2/6 bg-black flex">
+          <input placeholder="Site Name" type="text" className="h-10" onChange={e => setSiteName(e.target.value)}></input>
+          <button className="bg-yellow-500 h-10 px-10">Ok</button>
+          <button className="bg-lime-500 h-10 px-10" onClick={()=>setvis(false)}>cancel</button>
+        </div>
+      </div>:null} */}
       </div>
+
     </div>
   );
 }
 
-export default DesignYourSite;
+export default index;
